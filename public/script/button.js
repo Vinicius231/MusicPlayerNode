2aab7d5972ea1f8bd52880ec13bed640520b0848
Vinicius231 <viniciusmanoeloliveira@gmail.com>
2023-03-29 23:17:54 -0300

    Adicionado tempo e duração

--- public/script/button.js
+++ public/script/button.js
@@ -1,55 +1,87 @@
-var nameMusic = document.querySelector('.nameMusic')
-var artista = document.querySelector('.artista')
+var nameMusic = document.querySelector(".nameMusic");
+var artista = document.querySelector(".artista");
+var barra = document.querySelector(".barra");
+var duracao = document.querySelector(".duracao");
+var tempo = document.querySelector(".tempo");
 
-const url = 'http://localhost:3000/api'
-var opcao = 0
-var musica = 0
-var audio
-var data
+const url = "http://localhost:3000/api";
+var opcao = 0;
+var musica = 0;
+var audio;
+var data;
 
-async function getAllMusic(){
-    const response = await fetch(url)
+async function getAllMusic() {
+  const response = await fetch(url);
 
-    data = await response.json()
+  data = await response.json();
 
-    audio = new Audio(`../music/${data[musica].Arquivo}`)
-    nameMusic.innerHTML = data[musica].music
-    artista.innerHTML = data[musica].Artista
+  audio = new Audio(`../music/${data[musica].Arquivo}`);
+  update();
 }
-const play = ()=> {
-    nameMusic.innerHTML = data[musica].music
-    artista.innerHTML = data[musica].Artista
-    if(opcao == 0 )  {
-        audio.play()
-        opcao++
-    } else if (opcao == 1) {
-        audio.pause()
-        opcao--
-    }
-}
+const play = () => {
+  if (opcao == 0) {
+    audio.play();
+    opcao++;
+  } else if (opcao == 1) {
+    audio.pause();
+    opcao--;
+  }
+  update();
+};
 
-const next = ()=> {
-    audio.pause()
-    musica++
-    if(musica >= data.length) {
-        musica = 0
-    }
-    nameMusic.innerHTML = data[musica].music
-    artista.innerHTML = data[musica].Artista
-    audio = new Audio(`../music/${data[musica].Arquivo}`)
-    audio.play()
-}
+const next = () => {
+  audio.pause();
+  musica++;
+  if (musica >= data.length) {
+    musica = 0;
+  }
+  audio = new Audio(`../music/${data[musica].Arquivo}`);
+  audio.play();
+  update();
+};
 
-const before = ()=> {
-    audio.pause()
-    musica--
-    if(musica < 0) {
-        musica = data.length-1
-    }
-    nameMusic.innerHTML = data[musica].music
-    artista.innerHTML = data[musica].Artista
-    audio = new Audio(`../music/${data[musica].Arquivo}`)
-    audio.play()
-}
+const before = () => {
+  audio.pause();
+  musica--;
+  if (musica < 0) {
+    musica = data.length - 1;
+  }
+  audio = new Audio(`../music/${data[musica].Arquivo}`);
+  audio.play();
+  update();
+};
 
-getAllMusic()
+const update = () => {
+  nameMusic.innerHTML = data[musica].music;
+  artista.innerHTML = data[musica].Artista;
+};
+
+const time = () => {
+  let time = audio.currentTime;
+
+  let timeMinuto = Math.floor(time / 60);
+  let timeSegundo = Math.floor(time % 60);
+
+  let duration = audio.duration;
+
+  let minuto = Math.floor(duration / 60);
+  let segundo = Math.floor(duration % 60);
+
+  if (timeMinuto < 10) {
+    timeMinuto = "0" + timeMinuto;
+  }
+  if (timeSegundo < 10) {
+    timeSegundo = "0" + timeSegundo;
+  }
+
+  tempo.innerHTML = `${timeMinuto}:${timeSegundo}`;
+  duracao.innerHTML = `${minuto}:${segundo}`;
+
+  barra.value = time;
+  barra.max = duration;
+};
+
+setInterval(time, 1000);
+
+//3:48
+getAllMusic();
