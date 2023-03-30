3c629b8af5361e83be2de5f8c42c17c29d623111
Vinicius231 <viniciusmanoeloliveira@gmail.com>
2023-03-29 23:43:10 -0300

    Router and path

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const music = require('./music.json')
+const router = express.Router()
 
 app.use(express.static('public'))
 
-app.get('/',(req,res)=> {
-    res.sendFile(__dirname+'/public/page/index.html')
+router.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname+'/public/page/index.html'))
 })
 
-app.get('/api',(req,res)=> {
+router.get('/api',(req,res)=> {
     return res.json(music)
 })
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log('Funcionando')
-})
+app.listen(process.env.PORT || 3000)
+
+app.use(router)
